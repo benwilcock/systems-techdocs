2ca7ff63203e541be8ccbe398520ae4aa185e2c0
benwilcock <benwilcock@gmail.com>
2023-05-26 10:55:37 +0100

    adding first component

--- publish.sh
+++ publish.sh
@@ -5,7 +5,8 @@
 # --storage-name tap-techdocs \
 # --entity where-for-dinner/component/where-for-dinner
 # Build the docker image
-docker build . -t techdocs
+# docker build . -t techdocs --no-cache
 
 # Run the docer image to generate the site and upload it
-docker run --rm --env-file .env -v ./:/tmp/catalog/ techdocs /tmp/catalog/mkdocs.yaml awsS3 tap-techdocs
+# docker run --rm --env-file .env -v ./:/tmp/catalog/ techdocs /tmp/catalog/mkdocs.yaml awsS3 tap-techdocs
+docker run --rm --env-file .env -v ./:/tmp/catalog/ techdocs /tmp/catalog/catalog-info.yaml awsS3 tap-techdocs
